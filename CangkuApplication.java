919a2071dba5639ccce10401a255e12416f04bb4
ZiYi Ma <2284146456@qq.com>
2026-05-28 14:44:18 +0800

    Add files via upload

---
+++ CangkuApplication.java
@@ -0,0 +1,13 @@
+package com.cang.cangku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CangkuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CangkuApplication.class, args);
+    }
+
+}
